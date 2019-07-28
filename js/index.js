ad4d29a81a2d2c8fd56a753ad8267b94bb7958d0
Jack <JackBarber98@users.noreply.github.com>
2019-07-28 21:45:47 +0100

    Fixes hamburger menu on resizing

--- js/index.js
+++ js/index.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
   workButtonListener();
   toggleNavbar();
+  resizeNavbar();
 });
+
+var navPressed = false;
 
 function workButtonListener() {
   $('#work-button').mouseover(function() {
@@ -13,7 +16,6 @@ function workButtonListener() {
 }
 
 function toggleNavbar() {
-  var navPressed = false;
   $('#nav-icon').click(function() {
     navPressed = !navPressed;
     if (navPressed) {
@@ -35,4 +37,31 @@ function hideNavbar() {
   $('#navbar').animate({'left' : '-100%'}, 300);
   $('#nav-icon').addClass('fa-bars');
   $('#nav-icon').removeClass('fa-times');
+}
+
+function resizeNavbar() {
+  $(window).resize(function() {
+    if (window.innerWidth > 779) {
+      navPressed = false;
+      $('#navbar').css({
+        'font-size': 'initial',
+        'height': 'initial',
+        'padding': 'initial',
+        'top': 'initial',
+        'left': 'initial',
+        'width': '100vw'
+      });
+    } else {
+      navPressed = false;
+      $('#navbar').css({'font-size': '1.3rem',
+          'height': '100vh',
+          'padding': '1rem 0',
+          'width': '300px',
+          'top': '0',
+          'left': '-100%'
+        });
+    }
+    $('#nav-icon').addClass('fa-bars');
+    $('#nav-icon').removeClass('fa-times');
+  });
 }
